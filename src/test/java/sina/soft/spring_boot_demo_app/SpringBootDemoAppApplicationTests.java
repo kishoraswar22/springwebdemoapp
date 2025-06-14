54a1867a78ce5343384ce43bbc7f76bd1ce857e4
kishoraswar22 <136694063+kishoraswar22@users.noreply.github.com>
2025-06-14 16:19:56 +0530

    first commit

---
+++ src/test/java/sina/soft/spring_boot_demo_app/SpringBootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sina.soft.spring_boot_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
